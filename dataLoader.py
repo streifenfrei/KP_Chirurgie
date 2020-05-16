63adb03264b9a838c5151e698b2d90f6a2ec8326
Martin Lelis <martinlelis@yahoo.de>
2020-05-16 19:05:48 +0200

    implementing resnet encoder

--- dataLoader.py
+++ dataLoader.py
@@ -5,8 +5,6 @@ from albumentations.pytorch.transforms import img_to_tensor
 import glob
 import json
 
-
-
 # for image augmentation
 from albumentations import (
     HorizontalFlip,
@@ -30,16 +28,17 @@ import uuid
 from torch.utils.data import DataLoader
 
 class_name_to_id_ = {
-'__ignore__':-1,
-'_background_':0,
-'grasper':1,
-'scissors':2,
-'needle_holder ':3
+    '__ignore__': -1,
+    '_background_': 0,
+    'grasper': 1,
+    'scissors': 2,
+    'needle_holder ': 3
 }
 
 
 class OurDataLoader(Dataset):
-    def __init__(self, data_dir, transform=None, mode='train', task_type='segmentation', class_name_to_id = class_name_to_id_): 
+    def __init__(self, data_dir, transform=None, mode='train', task_type='segmentation',
+                 class_name_to_id=class_name_to_id_):
         '''
         constructor of OurDataLoader
         @ param:
@@ -48,7 +47,7 @@ class OurDataLoader(Dataset):
             3. mode: 'train' or 'test' (return label or not)
             4. task_type: 'pose' or 'segmentation' or 'both' (return which type of label)
         '''
-        self.all_json_name_list = find_all_json(data_dir) #json file name list
+        self.all_json_name_list = find_all_json(data_dir)  # json file name list
         self.transform = transform
         self.mode = mode
         self.task_type = task_type
@@ -65,51 +64,49 @@ class OurDataLoader(Dataset):
         get next image (w/o label)
         '''
         data_name = self.all_json_name_list[idx]
-        
-        image,shapes = load_image(data_name)
-        if self.task_type == 'segmentation': # not binary but 4 channels: 4 instruments
+
+        image, shapes = load_image(data_name)
+        if self.task_type == 'segmentation':  # not binary but 4 channels: 4 instruments
             label = load_mask(image.shape, shapes, self.class_name_to_id)
-            
+
             data = {"image": image, "label": label}
-            #augmented = self.transform(**data)
+            # augmented = self.transform(**data)
             augmented = data
             image, mask = augmented["image"], augmented["label"]
             if self.mode == 'train':
                 return img_to_tensor(image), torch.from_numpy(mask).long()
             else:
                 # TODO: why dose it looks like this?
-                return img_to_tensor(image), str(img_file_name)
-                
-                
+                return img_to_tensor(image), "sdf"
+
+
         elif self.task_type == 'pose':
             label = load_pose(data_name)
             data = {"image": image, "label": label}
             # TODO: test if works or not with pose information
-            #augmented = self.transform(**data)
+            # augmented = self.transform(**data)
             augmented = data
             image, pose = augmented["image"], augmented["label"]
-            
+
             # TODO: how to deal with pose label?
             if self.mode == 'train':
                 pass
             else:
                 pass
-                
+
         # TODO: how to deal with this part?
         elif self.task_type == 'both':
             pass
-
-
 
 
 def find_all_json(json_dir):
     '''
     find all the data in data dir
     '''
-    all_json_names = glob.glob(json_dir+'\\*.json')
+    all_json_names = glob.glob(json_dir + '//*.json')
     return all_json_names
 
-    
+
 def image_transform(p=1):
     return Compose([
         PadIfNeeded(min_height=100, min_width=100, p=1),
@@ -119,19 +116,19 @@ def image_transform(p=1):
         Normalize(p=1)
     ], p=p)
 
-     
-   
+
 def img_b64_to_arr(img_b64):
     '''
     convert base64 to image array
     reference: https://github.com/wkentaro/labelme/blob/master/labelme/cli/json_to_dataset.py
-    ''' 
+    '''
     img_data = base64.b64decode(img_b64)
     f = io.BytesIO()
     f.write(img_data)
     img_arr = np.array(PIL.Image.open(f))
     return img_arr
-    
+
+
 # TODO: test how it works    
 def load_image(json_file):
     data = json.load(open(json_file))
@@ -141,9 +138,9 @@ def load_image(json_file):
     return img, shapes
 
 
-#==================================#
+# ==================================#
 def polygons_to_mask(img_shape, points, shape_type=None,
-                  line_width=10, point_size=5):
+                     line_width=10, point_size=5):
     mask = np.zeros(img_shape[:2], dtype=np.uint8)
     mask = PIL.Image.fromarray(mask)
     draw = PIL.ImageDraw.Draw(mask)
@@ -151,8 +148,9 @@ def polygons_to_mask(img_shape, points, shape_type=None,
     assert len(xy) > 2, 'Polygon must have points more than 2'
     draw.polygon(xy=xy, outline=1, fill=1)
     mask = np.array(mask, dtype=bool)
-    return mask    
-    
+    return mask
+
+
 def shapes_to_label(img_shape, shapes, label_name_to_value):
     cls = np.zeros(img_shape[:2], dtype=np.int32)
     ins = np.zeros_like(cls)
@@ -176,43 +174,42 @@ def shapes_to_label(img_shape, shapes, label_name_to_value):
             mask = polygons_to_mask(img_shape[:2], points, shape_type)
             cls[mask] = cls_id
             ins[mask] = ins_id
-            
+
     return cls, ins
-#==================================#
-    
+
+
+# ==================================#
+
 # load the mask   
 def load_mask(img_shape, shapes, class_name_to_id):
-
     cls, ins = shapes_to_label(
-                img_shape=img_shape,
-                shapes=shapes,
-                label_name_to_value=class_name_to_id,
-            )
+        img_shape=img_shape,
+        shapes=shapes,
+        label_name_to_value=class_name_to_id,
+    )
     ins[cls == -1] = 0  # ignore it.
     return ins
 
-    
+
 if __name__ == '__main__':
     test1 = DataLoader(
-            dataset=OurDataLoader(data_dir=r'.\\dataset', transform=image_transform(p=1)),
-            shuffle=True,
-            batch_size=2,
-            pin_memory=torch.cuda.is_available()
-        )
+        dataset=OurDataLoader(data_dir=r'dataset', transform=image_transform(p=1)),
+        shuffle=True,
+        batch_size=1,
+        pin_memory=torch.cuda.is_available()
+    )
 
     for epoch in range(3):
         for step, (batchX, batchY) in enumerate(test1):
             print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
                   batchX.shape, '| batch y: ', batchY.shape)
     import matplotlib.pyplot as plt
-    fig=plt.figure(figsize=(8, 8))
+
+    fig = plt.figure(figsize=(8, 8))
     for step, (batchX, batchY) in enumerate(test1):
-        fig.add_subplot(1,2,1)
+        fig.add_subplot(1, 2, 1)
         plt.imshow(batchX.view(batchX.shape[1], batchX.shape[2], batchX.shape[3]).permute(1, 2, 0))
-        fig.add_subplot(1,2,2)
+        fig.add_subplot(1, 2, 2)
         plt.imshow(batchY.view(batchY.shape[1], batchY.shape[2]).permute(0, 1))
         plt.show()
         break
-    
-    
-    
