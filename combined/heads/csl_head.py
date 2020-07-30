fixing recall/precision in localistation evaluation

combined/heads/csl_head.py
@@ -61,8 +61,8 @@ class CSLHead(nn.Module):
                     score.append(self.threshold_list[i])
             storage.put_histogram("csl_localisation/treshold_score", torch.tensor(score), bins=len(threshold_score))
         epsilon = 10e-6
-        precision = true_positive / (true_positive + false_positive + epsilon)
-        recall = true_positive / (true_positive * false_negative + epsilon)
+        precision = float(true_positive) / (true_positive + false_positive + epsilon)
+        recall = float(true_positive) / (true_positive * false_negative + epsilon)
         f1 = 2 / ((1/(recall + epsilon)) + (1/(precision + epsilon)))
         storage.put_scalar("csl_localisation/precision", precision)
         storage.put_scalar("csl_localisation/recall", recall)