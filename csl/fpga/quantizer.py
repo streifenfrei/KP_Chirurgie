e91500556b00a79261675ed7acb234996dfc5588
Martin Lelis <martinlelis@yahoo.de>
2021-03-10 13:42:37 +0100

    measuring average inference time in quantizer. adding cuda

--- csl/fpga/quantizer.py
+++ csl/fpga/quantizer.py
@@ -1,3 +1,4 @@
+import time
 from argparse import ArgumentParser
 from copy import deepcopy
 
@@ -9,29 +10,29 @@ from csl.data_loader import OurDataLoader, image_transform_valid, train_val_data
 from csl.net import CSLNet
 from util.evaluate import get_threshold_score
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def evaluate(model, dataloader, use_cuda=False):
-    if use_cuda:
-        model = model.to("cuda")
+
+def evaluate(model, dataloader):
+    model = model.to(device)
     model.eval()
     total = 0
     loss = 0
     segmentation_accuracy = 0
     keypoints_f1 = 0
+    inference_time = 0
     for index, batch in enumerate(dataloader):
         inputs, target = batch
-        if use_cuda:
-            inputs = inputs.to("cuda")
-            target = target.to("cuda")
+        inputs = inputs.to(device)
+        target = target.to(device)
         target = target.permute(0, 3, 1, 2)
         target_segmentation, target_localisation = torch.split(target, [1, 4], dim=1)
         batch_size = inputs.shape[0]
         total += batch_size
+        start = time.time()
         output = model(inputs)
+        inference_time += time.time() - start
         output_segmentation, output_localisation = output
-        upsample = torch.nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-        output_segmentation = upsample(output_segmentation)
-        output_localisation = upsample(output_localisation)
         # loss
         weights = torch.where(target_localisation > 0.0001, torch.full_like(target_localisation, 5),
                               torch.full_like(target_localisation, 1))
@@ -56,7 +57,7 @@ def evaluate(model, dataloader, use_cuda=False):
         recall = float(true_positive) / (true_positive + false_negative + epsilon)
         keypoints_f1 += 2 / ((1 / (recall + epsilon)) + (1 / (precision + epsilon)))
         print("\revaluating {}/{}".format(index, len(dataloader)), end="")
-    print("")
+    print("average inference time: {}\n".format(inference_time / total))
     return segmentation_accuracy / total, keypoints_f1 / total, loss / total
 
 
@@ -69,7 +70,6 @@ if __name__ == "__main__":
     arg_parser.add_argument("--finetune", "-ft", action="store_true")
     arg_parser.add_argument("--dataset", "-ds", type=str, default=".")
     args = arg_parser.parse_args()
-    input = torch.randn((args.batch_size, 3, 512, 960))
     checkpoint = torch.load(args.weights, map_location=torch.device('cpu'))
     model = CSLNet()
     model.load_state_dict(checkpoint["model_state_dict"])
@@ -80,9 +80,9 @@ if __name__ == "__main__":
                             non_image_norm_flag=True)
     loader = train_val_dataset(dataset, validation_split=0, train_batch_size=args.batch_size,
                                valid_batch_size=args.batch_size, shuffle_dataset=True)[0]
-    use_cuda = False
     if args.quant_mode != "float":
-        quantizer = torch_quantizer(args.quant_mode, model, (input))
+        input = torch.randn((args.batch_size, 3, 512, 960))
+        quantizer = torch_quantizer(args.quant_mode, model, (input), device=torch.device(device))
         model = quantizer.quant_model
         if args.quant_mode == "calib":
             if args.finetune:
@@ -94,9 +94,8 @@ if __name__ == "__main__":
                 quantizer.load_ft_param()
             if args.deploy:
                 quantizer.export_xmodel(deploy_check=False)
-    elif torch.cuda.is_available():
-        use_cuda = True
 
-    segmentation_accuracy, keypoint_f1, loss = evaluate(model, loader, use_cuda=use_cuda)
-    print("segmentation accuracy: {}\n keypoint f1 score: {}\n loss: {}"
-          .format(segmentation_accuracy, keypoint_f1, loss))
+    if args.quant_mode != "calib" and not args.deploy:
+        segmentation_accuracy, keypoint_f1, loss = evaluate(model, loader)
+        print("segmentation accuracy: {}\n keypoint f1 score: {}\n loss: {}"
+              .format(segmentation_accuracy, keypoint_f1, loss))
